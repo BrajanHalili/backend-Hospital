1fc948d24711b66177ecebe25566a8e8d120f07d
rezkhan7 <73037837+rezkhan7@users.noreply.github.com>
2023-12-15 11:47:02 -0500

    Removed credentials

--- database/database.js
+++ database/database.js
@@ -6,7 +6,6 @@ require('dotenv').config()
 const db = new Sequelize("Hospital", "postgres", process.env.password, {
     host: 'localhost',
     dialect: 'postgres',
-    port: 5434
   })
 
 /**const db = new Sequelize({
